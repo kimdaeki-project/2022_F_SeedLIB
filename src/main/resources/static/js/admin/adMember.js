ded1cdc93878776024bb64284d74bc434f4e12f5
KOOHYO <zpfhzpfh112@naver.com>
2022-12-29 23:57:17 +0900

    계정 활성화 비활성화 수정 완료!

--- src/main/resources/static/js/admin/adMember.js
+++ src/main/resources/static/js/admin/adMember.js
@@ -1,37 +1,12 @@
 console.log("자바스크립트 연결");
-// function memberLocked(num){
-//     let userName = num;
-//     console.log("userName : ", userName);
-//     // let check = window.confirm("해당 계정을 비활성화 하시겠습니까?");
-//     // if(check){
-//     //     $("#lockedForm").submit();
-//     //     console.log("확인");
-//     // }else{
-//     //     console.log("취소");
-//     //     return;
-//     // }
-// };
-
-// $(".userList").on("click", ".userId", function(){
-//     console.log("클릭이벤트");
-//     let userName = $(this).attr("data-member-id");
-//     console.log("클릭한 멤버 아이디 : ", userName);
-// });
-
-// $(".userId").click(function(){
-
-//     console.log("클릭이벤트");
-// });
-
 $(document).on('click', '.lockId', function(e){
     e.preventDefault();
     let userName = $(this).attr("data-user-id");
-    console.log("유저아이디 : ", userName);
+    // 확인창
     let check = window.confirm("해당 계정을 비활성화 하시겠습니까?");
     if(check){
 
-        console.log("확인");
-
+        // 계정 비활성화 컬럼 0으로 업데이트
         $.ajax({
             type:"POST",
             url:"locked",
@@ -60,12 +35,11 @@ $(document).on('click', '.lockId', function(e){
 $(document).on('click', '.unLockId', function(e){
     e.preventDefault();
     let userName = $(this).attr("data-user-id");
-    console.log("계정활성화 유저아이디 : ", userName);
+    // 확인창
     let check = window.confirm("해당 계정을 활성화 하시겠습니까?");
     if(check){
 
-        console.log("확인");
-
+        // 계정 활성화 컬럼 1로 업데이트
         $.ajax({
             type:"POST",
             url:"unLocked",
